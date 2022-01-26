c2648f70abc5d1b65466733dad6591fa50399a59
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2022-01-26 12:59:04 +0600

    implement result page

--- src/components/ResultPage/ResultPage.jsx
+++ src/components/ResultPage/ResultPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {List} from "antd";
+import BasketCard from "../PaymentPage/BasketCard/BasketCard";
+import {connect} from "react-redux";
+import './ResultPage.css';
 
-const ResultPage = () => {
+const ResultPage = ({purchases}) => {
   return (
-    <div>
+    <div className='result-page-wrap'>
+      <div className='result-page'>
+        <h2 className='result-page-title'>Оплата проведена успешно</h2>
+        <List
+          bordered
+          dataSource={purchases}
+          renderItem={item => (
+            <BasketCard purchase={item} key={item.id}/>
+          )}
+        />
+      </div>
     </div>
   );
 };
 
-export default ResultPage;
+const mapStateToProps = (state) => {
+  return {
+    purchases: state.purchases,
+  }
+}
+
+export default connect(mapStateToProps, null)(ResultPage);
